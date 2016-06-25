df97c2431379f4b4e8f156777aebdb35167165c4
Luan Medeiros Macena <luanxmedeiros@live.com>
2016-06-24 21:11:48 -0300

    Primeira tentativa da consulta letra A

--- consultas.py
+++ consultas.py
@@ -1,6 +1,40 @@
 from appprod.models import *
+
+from appprod.models import *
+
+'''
 for processo in ProcessoProducao.objects.all():
-    lista = []
-    for prestador in processo.prestador.iterator():
-        lista.append(prestador.nome)
-        print('Processo: ',processo.descricao,  '\nPrestadores: ', lista)
+        prestadores = []
+        for prest in Prestador.objects.get(id=processo.id).nome:
+                prestadores.append(prest)
+        print("Processo:",processo.descricao.__str__(), prestadores)
+'''
+tudo = []
+for prest in Prestador.objects.all():
+    tudotemp = {"Prestador":"","Processo_Etapa":[]}
+    procetap = []
+    #, "Matéria Prima":[]
+    tudotemp["Prestador"] = prest.nome.__str__()
+    for proco in ProcessoProducao.objects.filter(prestador=prest):
+        procetap.append("Processo: "+proco.descricao.__str__()+". Etapas: ")
+        for etap in EtapaProducao.objects.filter(processo_producao=proco):
+            procetap.append(etap.descricao.__str__())
+        #for p in ProcessoProducao.objects.filter(prestador=proco.prestador):
+
+    tudotemp["Processo_Etapa"] = procetap
+     #lista.append(prestador.nome)
+    #print("Prestador: ",prest.nome, "Processos: ",processoss, "Etapas: ",etapas)
+
+    #print('Processo: ',processo.descricao,  '\nPrestadores: ', lista)
+    tudo.append(tudotemp)
+    tudotemp = {"Prestador":"","Processo_Etapa":[]}
+    procetap = []
+for x in tudo:
+    print (x,"\n")
+
+
+'''
+for processo in ProcessoProducao.objects.all():
+        print("Processo:", processo.descricao.__str__())
+
+'''
